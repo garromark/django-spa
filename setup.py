525dcf6314f4abc31b848a382c3916eae292d7f1
Mark Garro <garromark@gmail.com>
2013-08-20 22:52:00 -0400

    First attempt at setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='django-spa-boilerplate',
+    version='0.0.1',
+    description='Basic setup for single-page application based on PostgreSQL, Django, and Knockout.js',
+    author='Mark Garro',
+    author_email='garromark@gmail.com',
+    packages=['spa'],
+)
